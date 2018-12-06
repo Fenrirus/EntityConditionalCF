fa1288d6650afd4a74cd4ce2f626e015e4fb0b95
Marian Marian <fenrir55@gmail.com>
2018-12-06 20:38:00 +0100

    wersja 1

---
+++ EntityConditionalCF/EmplyeeDbcontext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EntityConditionalCF
+{
+    public class EmplyeeDbcontext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Map(p => p.Requires("IsTerminated").HasValue(false))
+                .Ignore(m => m.IsTerminated);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EntityConditionalCF/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EntityConditionalCF
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmplyeeDbcontext db = new EmplyeeDbcontext();
+            GridView1.DataSource = db.Employees.ToList();
+            GridView1.DataBind();
+        }
+
+    }
+}
